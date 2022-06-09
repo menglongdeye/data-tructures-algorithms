2cdf0727fcdcee080f6e6d30b1889c85e966ad74
conglongli <liconglong@163.com>
2022-06-09 10:37:54 +0800

    作业：数组的度

--- arrayList-LinkedList/list-demo/src/main/java/com/lcl/homework/HomeWork02.java
+++ arrayList-LinkedList/list-demo/src/main/java/com/lcl/homework/HomeWork02.java
@@ -35,4 +35,39 @@ public class HomeWork02 {
         return ans;
     }
 
+
+    /**
+     * 数组的度
+     * @param nums
+     * @return
+     */
+    public int findShortestSubArray(int[] nums) {
+        // key为数值，int[]数组长度为3，分别是数字个数、当前数值的第一个值的位置、最后一个值的位置
+        Map<Integer, Integer[]> map = new HashMap();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                Integer[] arr = map.get(nums[i]);
+                arr[0] = arr[0]+1;
+                arr[2] = i;
+                map.put(nums[i], arr);
+            }else{
+                Integer[] arr = new Integer[]{1, i, i};
+                map.put(nums[i], arr);
+            }
+        }
+        int ansCount = 0;
+        int minLen = 0;
+        for(Integer num : map.keySet()){
+            Integer[] arr = map.get(num);
+            if(arr[0] > ansCount){
+                ansCount = arr[0];
+                minLen = arr[2] - arr[1] + 1;
+            }else if(arr[0] == ansCount && arr[2]-arr[1] < minLen){
+                minLen = arr[2] - arr[1] + 1;
+            }
+        }
+        return minLen;
+
+    }
+
 }
