d192accfd730179e9d34015ed0d68564ea4d5eb7
conglongli <liconglong@163.com>
2022-07-17 23:53:09 +0800

    跳跃游戏 & 完全平方数

---
+++ arrayList-LinkedList/list-demo/src/main/java/com/lcl/homework/NumberOfJumps.java
@@ -0,0 +1,29 @@
+package com.lcl.homework;
+
+public class NumberOfJumps {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int numSquares(int n) {
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int minn = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                minn = Math.min(minn, f[i - j * j]);
+            }
+            f[i] = minn + 1;
+        }
+        return f[n];
+    }
+}
